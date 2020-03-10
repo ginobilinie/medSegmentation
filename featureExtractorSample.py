726487f5507cc483c5bfcdd47304722a0f312e85
no name <ginobilinie@users.noreply.github.com>
2020-03-10 02:07:07 -0500

    Create featureExtractorSample.py

---
+++ featureExtractorSample.py
@@ -0,0 +1,39 @@
+class MyInceptionFeatureExtractor(nn.Module):
+    def __init__(self, inception, transform_input=False):
+        super(MyInceptionFeatureExtractor, self).__init__()
+        self.transform_input = transform_input
+        self.Conv2d_1a_3x3 = inception.Conv2d_1a_3x3
+        self.Conv2d_2a_3x3 = inception.Conv2d_2a_3x3
+        self.Conv2d_2b_3x3 = inception.Conv2d_3a_3x3
+        self.Conv2d_3b_1x1 = inception.Conv2d_3b_3x3
+        self.Conv2d_4a_3x3 = inception.Conv2d_4a_3x3
+        self.Mixed_5b = inception.Mixed_5b
+        # stop where you want, copy paste from the model def
+
+    def forward(self, x):
+        if self.transform_input:
+            x = x.clone()
+            x[0] = x[0] * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
+            x[1] = x[1] * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
+            x[2] = x[2] * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
+        # 299 x 299 x 3
+        x = self.Conv2d_1a_3x3(x)
+        # 149 x 149 x 32
+        x = self.Conv2d_2a_3x3(x)
+        # 147 x 147 x 32
+        x = self.Conv2d_2b_3x3(x)
+        # 147 x 147 x 64
+        x = F.max_pool2d(x, kernel_size=3, stride=2)
+        # 73 x 73 x 64
+        x = self.Conv2d_3b_1x1(x)
+        # 73 x 73 x 80
+        x = self.Conv2d_4a_3x3(x)
+        # 71 x 71 x 192
+        x = F.max_pool2d(x, kernel_size=3, stride=2)
+        # 35 x 35 x 192
+        x = self.Mixed_5b(x)
+        # copy paste from model definition, just stopping where you want
+        return x
+
+inception = torchvision.models['inception_v3_google']
+my_inception = MyInceptionFeatureExtractor(inception)
